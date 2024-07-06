1a91def0e7d5d17942c74fb9c7e302451aeed50f
1kivanc <kivanc0556@gmail.com>
2024-07-07 01:47:27 +0300

    added exclude_patterns and redirected to /yorumlar

--- start.py
+++ start.py
@@ -1,5 +1,12 @@
+import os
 import time
+import re
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 
 def log(log_text):
     log_text = str(time.strftime("%Y.%m.%d %H:%M:%S")) + " ➾ " + log_text
@@ -8,32 +15,71 @@ def log(log_text):
     log_file.write(log_text + "\n")
     log_file.close()
 
-global_delay = 0.5
-driver = webdriver.Chrome()
-log('Bu program Can Tarafından Yapılmıştır.')
-log('https://fastuptime.com ve https://speedsmm.com üzerinden bize ulaşabilirsiniz.')
-log('Program başlatıldı')
+def trendyol_yorum_cek(url):
+    if os.path.exists("yorumlar.txt"):
+        os.remove("yorumlar.txt")
 
-urun_url =  input("Ürün Linki Sonuna / Koymayın: ") #'https://www.trendyol.com/trendypassion/sirt-pusula-baskili-tshirt-p-260271556'  Ürün URL NOT SONUNA / KOYMAYIN
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.get(url)
+    time.sleep(9) 
 
-try:
-    driver.get(urun_url + "/yorumlar")
-    time.sleep(5)
-    kac_yorum_var = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/div[1]/div/div[2]/span[2]').text
-    kac_yorum_var = kac_yorum_var.replace(" Yorum", "")
-    log('Toplam ' + kac_yorum_var + ' yorum var.')
-    for i in range(int(kac_yorum_var)):
-        try:
-            yorum = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div[2]/div/div[2]/div[3]/div[4]/div[' + str(i) + ']/div[1]/div/p').text
-            log('Yorum: '+ yorum)
-            yorum_file = open("yorumlar.txt", "a", encoding='utf-8')
-            yorum_file.write(yorum + "\n")
+    try:
+        total_comments = 0
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        time.sleep(5)
+        kac_yorum_var = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div/div/div/div[1]/div/div[2]/div[2]/div[3]/div').text
+        kac_yorum_var = kac_yorum_var.replace(" Yorum", "")
+        log('Toplam ' + kac_yorum_var + ' yorum var.')
+        while True:
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(global_delay)
-        except:
-            continue
-except Exception as e:
-    log('Hata: ' + str(e))
-    log('Program sonlandı')
-    driver.quit()
-    exit()
+            time.sleep(5) 
+
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+            html_content = driver.page_source
+            soup = BeautifulSoup(html_content, "html.parser")
+            comments = soup.find_all("p")
+
+            exclude_patterns = [
+                r"Acele et! Son \d+ günde \d+[\+\.,]? adet ürün satıldı!!",
+                r"\d+B kişinin sepetinde, tükenmeden al!",
+                r"Popüler ürün! Son \d+ saatte \d+B kişi görüntüledi!",
+                r"Sevilen ürün! \d+B kişi favoriledi!",
+                r"Sepetinizde Ürün Bulunmamaktadır.",
+                r"10.000’lerce yeni ürünü ve sezon trendlerini büyük indirimlerle yakalamak için,"
+            ]
+
+            def is_excluded(text):
+                for pattern in exclude_patterns:
+                    if re.search(pattern, text):
+                        return True
+                return False
+
+            with open("yorumlar.txt", "a", encoding='utf-8') as dosya:
+                for comment in comments:
+                    if not any(comment.has_attr(attr) for attr in ["class", "div", "span", "id"]):
+                        if not is_excluded(comment.text):
+                            dosya.write(comment.text + "\n")
+                            print(comment.text)
+                            total_comments += 1
+
+    except Exception as e:
+        log('Hata: ' + str(e))
+    finally:
+        log(f'Toplam {total_comments} yorum toplandı.')
+        log('Program sonlandı')
+        driver.quit()
+
+
+urun_url = input("Trendyol ürününün yorumlar sayfasının linkini giriniz: ")
+if '?' in urun_url:
+    urun_url = urun_url[:urun_url.index('?')]  
+urun_url += "/yorumlar"
+trendyol_yorum_cek(urun_url)
